db6c97e6d61f88e78434c5f857dce45c1be6c7b6
Master-mohit <mohitmansigupta9098@gmail.com>
2024-06-04 21:46:24 +0530

    grup anagram

--- praa/String.java
+++ praa/String.java
@@ -571,6 +571,39 @@ class Solution {
   return -1;
 }
 }
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // HashMap to store sorted string as key and list of anagrams as value
+        HashMap<String, List<String>> map = new HashMap<>(); 
+        
+        
+        if(strs == null || strs.length == 0){
+            return new ArrayList<>();
+        }
+        
+       
+        for(String str : strs){ 
+           
+            char [] ch = str.toCharArray(); 
+            Arrays.sort(ch); 
+            String sorted = new String(ch);
+            
+
+            if(!map.containsKey(sorted)){
+                map.put(sorted, new ArrayList<>());
+            }
+            
+            
+            map.get(sorted).add(str); 
+        }
+        
+      
+        return new ArrayList<>(map.values());
+    }
+}
+
     }
     
   }
