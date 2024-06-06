08a746c19e692bcdaa07db17bbe0cb7601cbd9a2
Master-mohit <mohitmansigupta9098@gmail.com>
2024-06-06 10:47:01 +0530

    number of 1 bit

--- praa/String.java
+++ praa/String.java
@@ -1,5 +1,6 @@
 package praa;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -648,17 +649,27 @@ public class String {
       //   return head;
 
       //06/05/24
-      class Solution {
-        public boolean isPowerOfTwo(int n) {
-           if (n <= 0){
-            return false;
-           }
-           while(n % 2 == 0){
-            n = n / 2;
-           }
-           return n == 1;
-        }
+    //   class Solution {
+    //     public boolean isPowerOfTwo(int n) {
+    //        if (n <= 0){
+    //         return false;
+    //        }
+    //        while(n % 2 == 0){
+    //         n = n / 2;
+    //        }
+    //        return n == 1;
+    //     }
+    // }
+
+      int n = 11;
+      int count = 0;
+     while(n != 0){
+      if(n % 2 == 1){
+        count++;
+      }
+
+     n = n / 2 ;
+     }
+     return count;
     }
-    }
-    
   }
