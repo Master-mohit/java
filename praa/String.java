a5fb3edebbfb29cb5caf2e0873c17cfcc7bbd99c
Master-mohit <mohitmansigupta9098@gmail.com>
2024-06-09 10:09:30 +0530

    subarray sum divide

--- praa/String.java
+++ praa/String.java
@@ -721,5 +721,23 @@ public class String {
 //         return false;
 //     }
 // }
+
+// 09/05/24....
+// class Solution {
+//   public int subarraysDivByK(int[] nums, int k) {
+//       Map<Integer, Integer> map = new HashMap<>();
+//       map.put(0, 1);
+//       int sum = 0, count = 0;
+//       for (int i = 0; i < nums.length; i++) {
+//           sum = (sum + nums[i]) % k;
+//           if (sum < 0) {
+//               sum += k;
+//           }
+//           count += map.getOrDefault(sum, 0);
+//           map.put(sum, map.getOrDefault(sum, 0) + 1);
+//       }
+//       return count;
+//   }
+// }
     }
   }
