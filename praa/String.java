3daf16bd282b91fadebc394c68905b7d9b833cb0
Master-mohit <mohitmansigupta9098@gmail.com>
2024-06-18 22:28:49 +0530

    826. Most Profit Assigning Work

--- praa/String.java
+++ praa/String.java
@@ -890,76 +890,99 @@ public class String {
       // }
 
       
-            StringBuilder roman = new StringBuilder();
+            // StringBuilder roman = new StringBuilder();
     
-            while (num >= 1000) {
-                roman.append("M");
-                num -= 1000;
-            }
+            // while (num >= 1000) {
+            //     roman.append("M");
+            //     num -= 1000;
+            // }
             
-            while (num >= 900) {
-                roman.append("CM");
-                num -= 900;
-            }
+            // while (num >= 900) {
+            //     roman.append("CM");
+            //     num -= 900;
+            // }
             
-            while (num >= 500) {
-                roman.append("D");
-                num -= 500;
-            }
+            // while (num >= 500) {
+            //     roman.append("D");
+            //     num -= 500;
+            // }
             
-            while (num >= 400) {
-                roman.append("CD");
-                num -= 400;
-            }
+            // while (num >= 400) {
+            //     roman.append("CD");
+            //     num -= 400;
+            // }
             
-            while (num >= 100) {
-                roman.append("C");
-                num -= 100;
-            }
+            // while (num >= 100) {
+            //     roman.append("C");
+            //     num -= 100;
+            // }
             
-            while (num >= 90) {
-                roman.append("XC");
-                num -= 90;
-            }
+            // while (num >= 90) {
+            //     roman.append("XC");
+            //     num -= 90;
+            // }
             
-            while (num >= 50) {
-                roman.append("L");
-                num -= 50;
-            }
+            // while (num >= 50) {
+            //     roman.append("L");
+            //     num -= 50;
+            // }
             
-            while (num >= 40) {
-                roman.append("XL");
-                num -= 40;
-            }
+            // while (num >= 40) {
+            //     roman.append("XL");
+            //     num -= 40;
+            // }
             
-            while (num >= 10) {
-                roman.append("X");
-                num -= 10;
-            }
+            // while (num >= 10) {
+            //     roman.append("X");
+            //     num -= 10;
+            // }
             
-            while (num >= 9) {
-                roman.append("IX");
-                num -= 9;
-            }
+            // while (num >= 9) {
+            //     roman.append("IX");
+            //     num -= 9;
+            // }
             
-            while (num >= 5) {
-                roman.append("V");
-                num -= 5;
-            }
+            // while (num >= 5) {
+            //     roman.append("V");
+            //     num -= 5;
+            // }
             
-            while (num >= 4) {
-                roman.append("IV");
-                num -= 4;
-            }
+            // while (num >= 4) {
+            //     roman.append("IV");
+            //     num -= 4;
+            // }
             
-            while (num >= 1) {
-                roman.append("I");
-                num -= 1;
-            }
+            // while (num >= 1) {
+            //     roman.append("I");
+            //     num -= 1;
+            // }
     
-            return roman.toString();
+            // return roman.toString();
+
+            
+            // 18/06/24
+
+            // int maxProfit = 0;
+            // Arrays.sort(worker);
+            // PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b) -> b[0]-a[0]);
+            // for(int i = 0 ; i< profit.length ; i++){
+            //     int[] temp = {profit[i] , i};
+            //     maxHeap.offer(temp);
+            // }
+    
+            // for(int i =worker.length-1 ;i>=0 ; i--){
+            //     while(!maxHeap.isEmpty()){
+            //         int[] temp = maxHeap.poll();
+            //         if(difficulty[temp[1]] <= worker[i]){
+            //             maxProfit += temp[0];
+            //             maxHeap.offer(temp);
+            //             break;
+            //         }
+            //     }
+            // }
+    
+            // return maxProfit;
         }
-  
 }
  
     
