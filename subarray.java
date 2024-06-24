c14c497438c7ecbf666674e76cb3103a946bb29d
Master-mohit <mohitmansigupta9098@gmail.com>
2024-06-24 22:57:07 +0530

    995. Minimum Number of K Consecutive Bit Flips

--- subarray.java
+++ subarray.java
@@ -1,42 +1,100 @@
 public class subarray {
     public static void main(String[] args) {
-        int count = 0; 
-        for (int i = 0; i < nums.length; i++) {
-            int oddCount = 0; 
-            for (int j = i; j < nums.length; j++) {
-                if (nums[j] % 2 != 0) {
-                    oddCount++; 
-                }
-                if (oddCount == k) {
-                    count++; 
-                }
-            }
-        }
+        // 22/06/24
+        // int count = 0; 
+        // for (int i = 0; i < nums.length; i++) {
+        //     int oddCount = 0; 
+        //     for (int j = i; j < nums.length; j++) {
+        //         if (nums[j] % 2 != 0) {
+        //             oddCount++; 
+        //         }
+        //         if (oddCount == k) {
+        //             count++; 
+        //         }
+        //     }
+        // }
 
-        return count; 
+        // return count; 
 
-        int maxLen = 1;
-        for (int left = 0; left < nums.length; left++) {
-            int min = nums[left];
-            int max = nums[left];
-            int temLen = 1;
-            if(maxLen >= nums.length -left ){
-                break;
-            }
-            for (int right = left + 1; right < nums.length; right++) {
-                max = Math.max(max, nums[right]);
-                min = Math.min(min, nums[right]);
-                if (Math.abs(max - min) <= limit) {
-                    temLen++;
-                    continue;
-                } else {
-                    break;
-                }
-            }
-            maxLen = Math.max(maxLen, temLen);
-        }
+        // 23/06/24
 
-        return maxLen;
+        // int maxLen = 1;
+        // for (int left = 0; left < nums.length; left++) {
+        //     int min = nums[left];
+        //     int max = nums[left];
+        //     int temLen = 1;
+        //     if(maxLen >= nums.length -left ){
+        //         break;
+        //     }
+        //     for (int right = left + 1; right < nums.length; right++) {
+        //         max = Math.max(max, nums[right]);
+        //         min = Math.min(min, nums[right]);
+        //         if (Math.abs(max - min) <= limit) {
+        //             temLen++;
+        //             continue;
+        //         } else {
+        //             break;
+        //         }
+        //     }
+        //     maxLen = Math.max(maxLen, temLen);
+        // }
+
+        // return maxLen;
+
+        // 24/06/24
+        // int flips=0;
+        
+        // int change=0;
+        // int n =nums.length;
+
+        
+        // int[] limit = new int[n];
+        // for(int i=0;i<n;i++)
+        // {
+           
+        //     nums[i]= (nums[i]+ change)%2;
+
+           
+        //     if(nums[i]==0)
+        //     {
+        //         if(i+k-1>=nums.length)
+        //         return -1;
+        //         else
+        //         limit[i+k-1]=1;  
+        //         change++;
+        //         flips++;
+        //     }
+            
+        //     change-=limit[i];
+        // }
+        // return flips;
+
+        // int flips=0;
+        
+        // int change=0;
+        // int n =nums.length;
+
+        
+        // int[] limit = new int[n];
+        // for(int i=0;i<n;i++)
+        // {
+           
+        //     nums[i]= (nums[i]+ change)%2;
+
+           
+        //     if(nums[i]==0)
+        //     {
+        //         if(i+k-1>=nums.length)
+        //         return -1;
+        //         else
+        //         limit[i+k-1]=1;  
+        //         change++;
+        //         flips++;
+        //     }
+            
+        //     change-=limit[i];
+        // }
+        // return flips;
 
     }
 }
