356af5f298b2ee3869e52a29e165a3b22dcbaefe
Master-mohit <mohitmansigupta9098@gmail.com>
2024-06-25 10:02:25 +0530

    1038. Binary Search Tree to Greater Sum Tree

--- subarray.java
+++ subarray.java
@@ -96,5 +96,44 @@ public class subarray {
         // }
         // return flips;
 
+
+        /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int val = 0;
+    
+    public TreeNode bstToGst(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        
+        if (root.right != null) {
+            bstToGst(root.right);
+        }
+        
+        val = root.val = val + root.val;
+        
+        if (root.left != null) {
+            bstToGst(root.left);
+        }
+        
+        return root;
+    }
+}
+
+
     }
 }
