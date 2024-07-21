59df5dfa551e04aa860f91dbb3d82167c0925f57
Master-mohit <mohitmansigupta9098@gmail.com>
2024-07-22 01:52:37 +0530

    217

--- subarray.java
+++ subarray.java
@@ -802,5 +802,20 @@ class Solution {
         return order.size() == k ? order : null;
     }
 }
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i =0;i<nums.length;i++){
+
+            if(set.contains(nums[i]))
+            return true;
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}
 //         }
     }
