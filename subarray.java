54a09584483a724ef63193de09fd5f8e341892d3
Master-mohit <mohitmansigupta9098@gmail.com>
2024-07-28 12:24:32 +0530

    594

--- subarray.java
+++ subarray.java
@@ -1014,6 +1014,23 @@ class Solution {
             return res;
         }
     }
+    class Solution {
+        public int findLHS(int[] nums) {
+            
+            Arrays.sort(nums);
+            int maxlength =0;
+            int start = 0;
+            for(int end =0; end <nums.length; end++){
+               while(nums[end ] - nums[start] > 1){
+                start++;
+               } 
+              if (nums[end] - nums[start] == 1){
+                maxlength =  Math.max(maxlength, end  - start + 1);
+               }
+            }
+            return maxlength;
+        }
+    }
 }
 }
     
