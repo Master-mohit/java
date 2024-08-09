d5ac97faeccf58bbee91cc6abf471bbdd1422a91
Master-mohit <mohitmansigupta9098@gmail.com>
2024-08-10 00:10:32 +0530

    840

--- praa/String.java
+++ praa/String.java
@@ -1286,6 +1286,55 @@ class Solution {
     }
 }
 
+class Solution {
+  public boolean isMagicSquare(int[][] grid, int r, int c){
+      boolean[] seen = new boolean[10];
+      for(int i=0;i<3;i++){
+          for(int j=0;j<3;j++){
+              int num=grid[r+i][c+j];
+              if(num<1||num>9||seen[num]){
+                  return false;
+              }
+              else
+                  seen[num]=true;;
+          }
+      }
+
+      int rSum=grid[r][c]+grid[r][c+1]+grid[r][c+2];
+      for(int i=0;i<3;i++){
+          // each row sum
+          if(grid[r+i][c]+grid[r+i][c+1]+grid[r+i][c+2]!=rSum){
+              return false;
+          }
+          // each col sum
+          if(grid[r][c+i]+grid[r+1][c+i]+grid[r+2][c+i]!=rSum){
+              return false;
+          }   
+      }
+      // diagonal
+      if(grid[r][c]+grid[r+1][c+1]+grid[r+2][c+2]!=rSum){
+          return false;
+      }
+      // antidiagonal
+      if(grid[r][c+2]+grid[r+1][c+1]+grid[r+2][c]!=rSum){
+          return false;
+      }
+      return true;
+  }
+  public int numMagicSquaresInside(int[][] grid) {
+      int rows=grid.length;
+      int cols=grid[0].length;
+      int count=0;
+      for(int i=0;i<=rows-3;i++){
+          for(int j=0;j<=cols-3;j++){
+              if(isMagicSquare(grid,i,j))
+                  count++;
+          }
+      }
+      return count;
+  }
+}
+
   }
 }
  
