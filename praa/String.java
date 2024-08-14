7d9dbd2150073f3999004136513c6349e4c1f79b
Master-mohit <mohitmansigupta9098@gmail.com>
2024-08-14 15:43:28 +0530

    719

--- praa/String.java
+++ praa/String.java
@@ -1479,31 +1479,56 @@ package praa;
 //     }
 // }
 
-class Solution {
-  public void solve(int[] candidates, int target,int idx, List<Integer> temp, List<List<Integer>> res, int sum) {
-      if (idx > candidates.length || sum > target) {
-          return;
-      }
-      if(sum == target) {
-          res.add(new ArrayList<>(temp));
-          return;
-      }
-      for (int j = idx; j < candidates.length; j++) {
-          if (j > idx && candidates[j] == candidates[j-1]) continue;
-          temp.add(candidates[j]);
-          solve(candidates, target, j+1, temp, res, sum+candidates[j]);
-          temp.remove(temp.size()-1);
-      }
+// class Solution {
+//   public void solve(int[] candidates, int target,int idx, List<Integer> temp, List<List<Integer>> res, int sum) {
+//       if (idx > candidates.length || sum > target) {
+//           return;
+//       }
+//       if(sum == target) {
+//           res.add(new ArrayList<>(temp));
+//           return;
+//       }
+//       for (int j = idx; j < candidates.length; j++) {
+//           if (j > idx && candidates[j] == candidates[j-1]) continue;
+//           temp.add(candidates[j]);
+//           solve(candidates, target, j+1, temp, res, sum+candidates[j]);
+//           temp.remove(temp.size()-1);
+//       }
 
-      return;
-  }
-  public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-      List<List<Integer>> res = new ArrayList<>();
-      Arrays.sort(candidates);
-      solve(candidates, target, 0, new ArrayList<>(), res, 0);
-      return res;
-  }
-}
+//       return;
+//   }
+//   public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+//       List<List<Integer>> res = new ArrayList<>();
+//       Arrays.sort(candidates);
+//       solve(candidates, target, 0, new ArrayList<>(), res, 0);
+//       return res;
+//   }
+// }
+// class Solution {
+//   public int smallestDistancePair(int[] nums, int k) {
+//       Arrays.sort(nums);
+//       int n = nums.length;
+//       int low = 0;
+//       int high = nums[n - 1] - nums[0];
+//       while (low < high) {
+//           int mid = (low + high) / 2;
+//           int count = 0;
+//           int left = 0;
+//           for (int right = 0; right < n; right++) {
+//               while (nums[right] - nums[left] > mid) {
+//                   left++;
+//               }
+//               count += right - left;
+//           }
+//           if (count < k) {
+//               low = mid + 1;
+//           } else {
+//               high = mid;
+//           }
+//       }
+//       return low;
+//   }
+// }
 
 //   }
 // }
