101da719c6bbfeec26aa5135f8084acb76af61c0
Master-mohit <mohitmansigupta9098@gmail.com>
2024-08-19 23:50:44 +0530

    650

--- praa/String.java
+++ praa/String.java
@@ -1634,6 +1634,41 @@ class Solution {
     }
 }
 
+class Solution {
+    public int minSteps(int n) {
+       if(n==0){
+        return 0;
+       }
+       if(n==1){
+        return 0;
+       } 
+       if(n==2){
+        return 2;
+       }
+       int[]t=new int[n+1];
+       //t[i]=min operations required to print i A's in notepad
+       t[0]=0;
+       t[1]=0;
+       t[2]=2;
+       for(int i=3;i<=n;i++){
+        int factor=i/2;
+        while(factor>0){
+            if(i%factor==0){
+               int steps_to_print_factor_As=t[factor];
+               int copy=1;
+               int paste=(i/factor)-1;
+               t[i]=steps_to_print_factor_As+copy+paste;
+               break;
+            }
+            else{
+                factor--;
+            }
+        }
+       }
+       return t[n];
+    }
+}
+
 //   }
 // }
  
