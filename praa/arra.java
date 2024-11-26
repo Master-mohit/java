fba06d23ae0c4ca1946d7d2b7cdaa55c64415307
Master-mohit <mohitmansigupta9098@gmail.com>
2024-11-26 18:15:43 +0530

    multiple string

--- praa/arra.java
+++ praa/arra.java
@@ -286,3 +286,14 @@ class Solution {
         }
     }
 }
+
+import java.math.BigInteger;
+class Solution {
+    public String multiply(String num1, String num2) {
+        
+        BigInteger a=new BigInteger(num1);
+        BigInteger b=new BigInteger(num2);
+        return a.multiply(b).toString();
+
+    }
+}
