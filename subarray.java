9edfd7690bf3b5c7446388743e3c9e95feb0c34d
Master-mohit <mohitmansigupta9098@gmail.com>
2024-12-07 23:29:04 +0530

    Reverse Linked List II

--- subarray.java
+++ subarray.java
@@ -1351,3 +1351,39 @@ class Solution {
         return dp[idx] = false;
     }
 }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummy=new ListNode();
+        dummy.next=head;
+        ListNode leftprev=dummy;
+        ListNode curr=head;
+        for(int i=0; i<left-1;i++){
+            leftprev=leftprev.next;
+            curr=curr.next;
+        }
+        ListNode reminder=curr;
+        ListNode prev=null;
+        for(int i=0; i<=right-left; i++){
+        ListNode nextnode=curr.next;
+        curr.next=prev;
+        prev=curr;
+        curr=nextnode;
+        }
+        leftprev.next=prev;
+        reminder.next=curr;
+        return dummy.next;
+        
+        
+    }
+}
