7c108174332b68df742ac07980b3c12ae4e20d4a
Master-mohit <mohitmansigupta9098@gmail.com>
2024-12-17 12:38:09 +0530

    492

--- praa/arra.java
+++ praa/arra.java
@@ -2237,3 +2237,13 @@ class Solution {
      * int param_3 = obj.peek();
      * boolean param_4 = obj.empty();
      */
+
+
+     class Solution {
+        public int[] constructRectangle(int area) {
+            int w=(int)Math.sqrt(area);
+            while(area%w>0)
+                w--;
+                return new int[] {area/w,w};
+        }
+    }
