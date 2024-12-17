398eae9190b903f5f8d56ae7c8b481e7d10d5f62
Master-mohit <mohitmansigupta9098@gmail.com>
2024-12-17 12:39:39 +0530

    504

--- praa/arra.java
+++ praa/arra.java
@@ -2247,3 +2247,11 @@ class Solution {
                 return new int[] {area/w,w};
         }
     }
+
+    import java.math.*;
+class Solution {
+    public String convertToBase7(int num) {
+        BigInteger m=new BigInteger(""+num);
+        return m.toString(7);
+    }
+}
