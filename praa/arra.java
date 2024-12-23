d7ed51c6adf5f41daab2c2abd5edb74057051ff5
Master-mohit <mohitmansigupta9098@gmail.com>
2024-12-23 20:42:36 +0530

    badlaav

--- praa/arra.java
+++ praa/arra.java
@@ -2271,3 +2271,25 @@ class Solution {
        return true;
     }
 }
+
+
+class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+
+        int n = grid.length, m = grid[0].length;
+
+        int[][] t = new int[n][m];
+        k %= n * m;
+        k = n * m - k;
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                t[i][j] = grid[(i + (j + k) / m) % n][(j + k) % m];
+
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int[] r : t)
+            ans.add(Arrays.stream(r).boxed().collect(Collectors.toList()));
+        
+        return ans;
+    }
+}
