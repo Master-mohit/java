c6ea4cca7d63075aef147df1463383594552204c
Master-mohit <mohitmansigupta9098@gmail.com>
2025-01-04 19:54:46 +0530

    744

--- praa/arra.java
+++ praa/arra.java
@@ -2346,3 +2346,14 @@ class Solution {
         return result;
     }
 }
+
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+
+        for (char letter : letters)
+            if (letter > target)
+                return letter;
+
+        return letters[0];
+    }
+}
